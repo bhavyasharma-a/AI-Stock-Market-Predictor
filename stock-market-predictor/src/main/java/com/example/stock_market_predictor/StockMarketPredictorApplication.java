7d2caaff3d1f6a248b941c521ec42a81d18bad8f
Bhavya <bhavya.sharma1@ucdconnect.ie>
2025-03-11 07:35:37 +0530

    Initialized Spring Boot project

---
+++ stock-market-predictor/src/main/java/com/example/stock_market_predictor/StockMarketPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_market_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketPredictorApplication.class, args);
+	}
+
+}
