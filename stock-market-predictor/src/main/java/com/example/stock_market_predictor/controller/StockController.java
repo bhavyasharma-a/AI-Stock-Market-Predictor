0fdab3be44f354db104f7a4e0668481fbd19d951
bhavyasharma-a <bhavyasharma037@gmail.com>
2025-03-13 12:12:34 +0530

    backend configured

--- stock-market-predictor/src/main/java/com/example/stock_market_predictor/controller/StockController.java
+++ stock-market-predictor/src/main/java/com/example/stock_market_predictor/controller/StockController.java
@@ -4,10 +4,14 @@ import com.example.stock_market_predictor.model.Stock;
 import com.example.stock_market_predictor.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/stocks")
 public class StockController {
